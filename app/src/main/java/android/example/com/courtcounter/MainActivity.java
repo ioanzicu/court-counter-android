d7f034fb2b5592f899d00bb4dc13a614a64012dc
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-28 01:30:41 +0100

    add java methods for team b

--- app/src/main/java/android/example/com/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/com/courtcounter/MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(0);
+        displayForTeamB(0);
     }
 
     /**
@@ -44,6 +46,39 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    /**
+     * Increase the score for Team B by 3 points
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 1 point
+     */
+    public void addOneForTeamB(View view) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
